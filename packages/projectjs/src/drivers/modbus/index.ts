555baaad8efa6c20a51d9513656e6f0d95cd4a18
kilianstallz <kp.stallinger@gmail.com>
2019-12-20 14:17:11 +0100

    publish pv energy

--- packages/projectjs/src/drivers/modbus/index.ts
+++ packages/projectjs/src/drivers/modbus/index.ts
@@ -1,6 +1,7 @@
 import * as modbus from 'jsmodbus'
 import chalk from 'chalk'
 import { Socket } from 'net'
+import { mqtt } from '../../app'
 const socket = new Socket()
 const client = new modbus.client.TCP(socket)
 
@@ -13,6 +14,7 @@ export function connectModbus(deviceIP?: string, devicePort?: number): void {
     () => {
       console.log(chalk.green('Connected'))
       setInterval(() => {
+        let prodDay: number, prodNow: number
         client
           .readHoldingRegisters(499, 2)
           .then(res => {
@@ -20,6 +22,7 @@ export function connectModbus(deviceIP?: string, devicePort?: number): void {
             console.log(chalk.blue('PV-Panel: ', productionWatt + 'W'))
             if (productionWatt > 1) {
               const print = Math.round((productionWatt / 12) * 52) // Hochrechnung auf 52 gleiche Module
+              prodNow = print
               console.log(chalk.blue('PV-Produktion: ', print + 'W'))
             }
           })
@@ -28,8 +31,10 @@ export function connectModbus(deviceIP?: string, devicePort?: number): void {
           })
         client.readHoldingRegisters(501, 4).then(res => {
           const productionWatt = res.response.body.valuesAsBuffer.readBigUInt64BE(0).toString()
+          prodDay = parseInt(productionWatt)
           console.log(chalk.blue(`Produktion Heute: ${productionWatt}Wh`))
         })
+        mqtt.publish('energie/solar', JSON.stringify({ productionDay: prodDay, productionNow: prodNow, time: Date.now() }))
       }, 20000)
     },
   )
